cd111ebf9be30cb6aee48d14fac9ea8d33b6f89d
jasenkaz <jasenkaz@yahoo.com>
2017-06-13 18:45:13 -0400

    commit after fixing and adding npm json package

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,12 @@
+/* jshint esversion: 6*/
   const Awesomeapptodoagain2 = {
-  rootElement = '#app',
-  todos = [],
-  start: function(){
-  this.cacheDOM();
-  this.bindEvents();
-  this.render();
-},
+    rootElement: '#app',
+    todos: [],
+    start: function(){
+    this.cacheDOM();
+    this.bindEvents();
+    this.render();
+  },
 
   cacheDOM: function(){
     this.root = document.querySelector(this.rootElement);
@@ -18,38 +19,38 @@
     this.createForm.addEventListener('submit', (event) => this.addTodo(event));
   },
   addTodo: function(event){
-event.preventDefault();
-const taskValue = this.taskInput.value;
-if(!taskValue){
-  return;
-}
-const todo = {
-  task: taskValue,
-  isComplete: false
-};
+    event.preventDefault();
+    const taskValue = this.taskInput.value;
+    if(!taskValue){
+      return;
+    }
+    const todo = {
+      task: taskValue,
+      isComplete: false
+    };
 
-this.todos.push(todo);
-this.render();
-this.taskInput.value = '';
+    this.todos.push(todo);
+    this.render();
+    this.taskInput.value = '';
   },
 
-cacheDeleteButtons: function(){
-  this.deleteButtons = this.root.querySelectorAll('.delete');
-},
+  cacheDeleteButtons: function(){
+    this.deleteButtons = this.root.querySelectorAll('.delete');
+  },
 
-bindDeleteEvents: function(){
-  this.deleteButtons.forEach((button, index) => {
-    button.addEventListener('click', () => this.deleteTodo(index));
-});
-},
-deleteTodo: function(index){
-  this.todos.splice(index, 1);
-  this.render();
-},
+  bindDeleteEvents: function(){
+    this.deleteButtons.forEach((button, index) => {
+      button.addEventListener('click', () => this.deleteTodo(index));
+    });
+  },
+  deleteTodo: function(index){
+    this.todos.splice(index, 1);
+    this.render();
+  },
 
   render: function(){
     const lis = this.todos
-              .map(todo => '<li>${todo.task}<button class = 'delete'>X</button></li>')
+              .map(todo => `<li>${todo.task}<button class = 'delete'>X</button></li>`)
               .join('');
     this.todoList.innerHTML = lis;
     this.cacheDeleteButtons();
